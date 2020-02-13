93605aa9b38f14778e381e4bfbfb2c476727ec75
PC-27\user <ptanh2212@gmail.com>
2020-02-13 16:44:29 +0700

    Add Cinemachine

---
+++ CHAOS/Assets/Test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+    GameObject test;
+    // Use this for initialization
+    void Start () {
+        test = GameObject.FindGameObjectWithTag("Finish");
+    }
+	
+	// Update is called once per frame
+	void Update() {
+        Debug.DrawRay(test.transform.position, test.transform.forward * 1000, Color.green);
+    }
+}
