d0794e3def8406e6739db4a2edf49e8c37c47976
PC-27\user <ptanh2212@gmail.com>
2020-02-19 11:54:22 +0700

    Camera setting

--- CHAOS/Assets/Scripts/Camera/CameraSettings.cs
+++ CHAOS/Assets/Scripts/Camera/CameraSettings.cs
@@ -10,6 +10,7 @@ namespace Gamekit3D
     {
         private float defaultFOV;
         private Player playerScript;
+        [SerializeField] private float fovOffset;
         public enum InputChoice
         {
             KeyboardAndMouse, Controller,
@@ -114,7 +115,7 @@ namespace Gamekit3D
             float num = this.playerScript.limit.maxHorizontal / 2f + run;
             float num2 = Mathf.Clamp((this.playerScript.VelocityMagnitude - run) / num, 0f, 1f);
             //float b = this.defaultFOV + 10f * num2;
-            float b = this.defaultFOV + 50f * num2;
+            float b = this.defaultFOV + fovOffset * num2;
             //return Mathf.Lerp(this.myCamera.fieldOfView, b, LERP);
             return Mathf.Lerp(Current.m_Lens.FieldOfView, b, LERP);
         }
